99e28e8dca30badb7f6b710c35e62434b46c6813
sahil patel <59383203+sahil-patel001@users.noreply.github.com>
2022-03-23 17:58:54 +0530

    Delete backend/routes directory

--- backend/routes/couponRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { generateCode } = require("../controllers/couponCodeController");
-const { protect } = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/generate-coupon-code", protect, generateCode)
-
-module.exports = router
